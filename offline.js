c96d7ccccaff78c6f73abe7888d2a4e19ea9923c
Aibaraichigo <61005957+Aibaraichigo@users.noreply.github.com>
2020-02-14 08:51:32 +0700

    My word puzzle game

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1581644445,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/line1-sheet0.png",
+		"images/line2-sheet0.png",
+		"media/candy-catcher-1.m4a",
+		"media/candy-catcher-1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
